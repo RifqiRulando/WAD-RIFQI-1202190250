a7f1353c81809df1d8b8d11f38079735093124e2
RifqiRulando <kiki.rulando@gmail.com>
2021-10-24 23:51:12 +0700

    first commit

---
+++ Modul 2/Booking.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Booking</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class=" collapse navbar-collapse justify-content-center">
+            <ul class="navbar-nav ">
+
+                <li class="nav-item">
+                    <a class=" nav-link " href="Home.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class=" nav-link" href="Booking.php">Booking<span class="sr-only">(current)</span></a>
+                </li>
+
+            </ul>
+        </div>
+
+    </nav>
+    <br><br>
+
+    <!--phpsection-->
+    <?php
+        $method_selected = '';
+        $image_selected = '';
+        $standard_bk = isset($_POST['standard_book']);
+        $superior_bk = isset($_POST['superior_book']);
+        $luxury_bk = isset($_POST['luxury_book']);
+        $img_src = [
+            "1.png",
+            "2.png",
+            "3.png"
+        ];
+
+        // Booking from Book Now buttons
+        if ($standard_bk) {
+            $method_selected = '
+                <select class="custom-select" name="roomtype" disabled>
+                <option value="standard">Standard</option>
+                <input type="hidden" name="roomtype" value="standard">
+                </select>';
+            $image_selected = $img_src[0];
+        } else if ($superior_bk){
+            $method_selected = '
+                <select class="custom-select" name="roomtype" disabled>
+                <option value="superior">Superior</option>
+                <input type="hidden" name="roomtype" value="superior">
+                </select>';
+            $image_selected = $img_src[1];
+        }else if ($luxury_bk){
+            $method_selected = '
+                <select class="custom-select" name="roomtype" disabled>
+                <option value="luxury">Luxury</option>
+                <input type="hidden" name="roomtype" value="luxury">
+                </select>';
+            $image_selected = $img_src[2];
+        //The other method
+        }else {
+            $method_selected = '
+                <select class="custom-select" name="roomtype">
+                <option value="standard">Standard</option>
+                <option value="superior">Superior</option>
+                <option value="luxury">Luxury</option>
+                </select>';
+            $image_selected = $img_src[0];
+        }
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+
+                <form action="Mybooking.php" method="get">
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <label for="inputNama">Name</label>
+                            <input type="text" class="form-control" id="validationCustom01" name="Nama">
+
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <label for="inputNama">Event Date</label>
+                            <input type="date" class="form-control" id="validationCustom01" name="CheckIn">
+
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <label for="inputNama">Duration (days)</label>
+                            <input type="type" class="form-control" id="validationCustom01" name="Durasi">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <?php
+				   				if (isset($_GET['pindah'])) {
+				   					$pindah =$_GET['pindah'];
+				   				}else{
+				   					$pindah = '';
+				   				}
+				   			?>
+                            <div class="form-group">
+
+                                <label for="inputNama">Building Type</label>
+                                <?php if ($pindah == '') {?>
+                                <select class="custom-select" name="RoomType">
+                                    <option selected>Building..</option>
+                                    <option>Peta Park</option>
+                                    <option>Bandung Convention Center</option>
+                                    <option>Balai Sartika</option>
+                                </select>
+                                <?php  }else{ ?>
+                                <input type="text" class="form-control" readonly="" name="RoomType"
+                                    value="<?php echo($_GET['pindah']) ?>">
+                                <?php } ?>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <label for="inputNama">Add Service(s)</label>
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                                $ 20/service
+                            </small>
+                            <div class="form-check form-check">
+                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
+                                    value="Room Service" name="AddService[]" value="Room Service ">
+                                <label class="form-check-label" for="inlineCheckbox1">Room Service</label>
+                            </div>
+                            <div class="form-check form-check">
+                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="Breakfast"
+                                    name="AddService[]" value="Breakfast">
+                                <label class="form-check-label" for="inlineCheckbox1">Breakfast</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <label for="inputNama">Phone Number</label>
+                            <input type="type" class="form-control" id="validationCustom01" name="PhoneNumber">
+
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-10 mb-3">
+                            <input type="submit" name="submit" value="Book"
+                                class="btn btn-primary btn-lg btn-block mt-1 ">
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <br>
+            <br>
+            <div class="col-md-6">
+                <img src="" style="width: 110%; height: 350px">
+
+            </div>
+        </div>
+
+    </div>
+    <!--Footer-->
+        <!-- Copyright -->
+        <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            Dibuat oleh:
+            <a class="text-white">Rifqi Rulando_1202190250</a>
+        </div>
+        <!-- Copyright -->
+        </footer>
+</body>
+
+</html>
---
+++ Modul 2/a.html
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Rifqi_1202190250</title>
+</head>
+
+<body>
+
+  <!--NAVBAR-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class=" collapse navbar-collapse justify-content-center">
+      <ul class="navbar-nav ">
+
+        <li class="nav-item active">
+          <a class=" nav-link " href="Home.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+
+        <li class="nav-item">
+          <a class=" nav-link" href="Booking.php">Booking<span class="sr-only">(current)</span></a>
+        </li>
+
+      </ul>
+    </div>
+
+    <!--TEXT JUDUL-->
+  </nav>
+  <br>
+  <nav class="container">
+    <nav class="row text-center">
+      <nav class="col-md-12">
+        <h5>WELCOME TO ESD VENUE RESERVATION</h5>
+      </nav>
+    </nav>
+    <br>
+
+    <nav class="container">
+      <nav class="row text-center text-dark">
+        <nav class="col-md-12">
+          Find your best deal for your event, here!
+        </nav>
+      </nav>
+      <br>
+
+      <!--PHP SECTION-->
+      <?php 
+        $img_src = [
+          "1.png", 
+          "2.png", 
+          "3.png"
+        ];
+        ?>
+
+<!--CARD-->
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card shadow-lg" style="width: auto">
+          <img src="<?=$bandung?>.jpg" class="card-img-top"  alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title text-left"><b>Bandung Techno Park</b></h5>
+            <p class="text-left">Outdoor/Indoor</p>
+            <p class="card-text text-primary text-left"><b>Rp.12.000.000/Day</b></p>
+            <p class="text-left">3000 Capacity</p>
+            <div class="card-header">
+              <b>FACILITIES</b>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item text-success">Free Parking Space</li>
+              <li class="list-group-item text-success">Full AC</li>
+              <li class="list-group-item text-success">Cleaning Services</li>
+              <li class="list-group-item text-success">Covid-19 Protocols</li>
+            </ul>
+            <div class="card-footer">
+           <a href="Booking.php?room=Standard" class="btn btn-primary btn-block">Book Now</a>
+            </div>
+          </div>
+       </div>
+    </div> 
+
+    <div class="col-md-4">
+      <div class="card shadow-lg" style="width: auto">
+          <img src=<?=$img_src[1]?> class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title text-left"><b>Gedung Serba Guna</b></h5>
+            <p class="text-left">Indoor</p>
+            <p class="card-text text-primary text-left"><b>Rp.14.000.000/Day</b></p>
+            <p class="text-left">4500 Capacity</p>
+            <div class="card-header ">
+              <b>FACILITIES</b>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item text-danger">Free Parking Space</li>
+              <li class="list-group-item text-success">Full AC</li>
+              <li class="list-group-item text-success">Cleaning Services</li>
+              <li class="list-group-item text-success">Covid-19 Protocols</li>
+            </ul>
+          
+           <div class="card-footer">
+           <a href="Booking.php?room=VIP" class="btn btn-primary btn-block">Book Now</a>
+            </div>
+          </div>
+       </div>
+    </div>  
+
+    <div class="col-md-4">
+      <div class="card shadow-lg" style="width: auto">
+          <img src=<?=$img_src[2]?> class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title text-left"><b>Gedung Keong</b></h5>
+            <p class="text-left">Indoor</p>
+            <p class="card-text text-primary text-left"><b>Rp.17.000.000/Day</b></p>
+            <p class="text-left">7000 Capacity</p>
+            <div class="card-header">
+              <b>FACILITIES</b>
+            </div>
+            <ul class="list-group list-group-flush">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item text-danger">Free Parking Space</li>
+                <li class="list-group-item text-danger">Full AC</li>
+                <li class="list-group-item text-success">Cleaning Services</li>
+                <li class="list-group-item text-success">Covid-19 Protocols</li>
+            </ul>
+
+            <div class="card-footer">
+              <a href="Booking.php?room=VIP" class="btn btn-primary btn-block">Book Now</a>
+               </div>
+             </div>
+          </div>
+       </div>
+       <!--Footer-->
+        <!-- Copyright -->
+        <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            Dibuat oleh:
+            <a class="text-white">Rifqi Rulando_1202190250</a>
+        </div>
+        <!-- Copyright -->
+        </footer>
+
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+      </script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+      </script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+      </script>
+</body>
+
+</html>
