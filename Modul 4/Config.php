73e4a5e250d9e6019aac72ac43c17f1b4bef362b
darisrd <55936213+darisrd@users.noreply.github.com>
2021-12-03 16:37:19 +0700

    debug m4

--- Modul 4/Config.php
+++ Modul 4/Config.php
@@ -1,14 +1,14 @@
-<?php
-// if(!isset($_SESSION)){
-//     session_start();
-// }
-$conn=mysqli_connect("localhost:3307", "root", "", "modul4");
+    <?php
+    // if(!isset($_SESSION)){
+    //     session_start();
+    // }
+    $conn=mysqli_connect("localhosst:3307", "root", "", "modul4");
 
 
-if(!$conn){
-    echo "<script>
-            alert ('dissconect');
-            </script>";
-    die("conection failed." . mysqli_connect_error());
-}
-?>
+    if(!$conn){
+        echo "<script>
+                alert ('dissconect');
+                </script>";
+        die("conection failed." . mysqli_connect_error());
+    }
+    ?>
